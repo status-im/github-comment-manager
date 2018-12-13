749b946fd27121fca962148c5e09358d7fd463c1
Jakub Sokołowski <jakub@status.im>
2018-12-13 10:06:14 +0100

    add separator in table between build numbers
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- src/template.js
+++ src/template.js
@@ -3,6 +3,9 @@ module.exports = `
 | :grey_question: | Commit | :hash: | Finished | Duration | Platform | Result |
 |-|-|-|-|-|-|-|
 {% for b in builds -%}
+{%- if b.id > builds[loop.index0-1].id -%}
+| | | | | | | |
+{% endif -%}
 {%- if b.success -%}
 | :heavy_check_mark: | {{ b.commit }} | [#{{ b.id }}]({{ b.url }}) | {{ b.meta.created | date }} | {{ b.duration }} | \`{{ b.platform }}\` | [:package: package]({{ b.pkg_url }}) |
 {% else -%}
