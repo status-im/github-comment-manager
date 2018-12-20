3d43fb18ba5eb8198434550a2f07395da69a61b7
Jakub Sokołowski <jakub@status.im>
2018-12-20 22:18:22 +0100

    limit logging noise fo tests
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,10 @@
+import log from 'loglevel'
 import chai from 'chai'
 import sinonChai from 'sinon-chai'
 import chaiAsPromised from 'chai-as-promised'
+
+/* limit amount of console noise for tests */
+log.setDefaultLevel(log.levels.WARN)
 
 chai.use(sinonChai)
 chai.use(chaiAsPromised);
